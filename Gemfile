a08544f149068a1aea5bb05ae199a38dbac973be
Elochai <buu9992@gmail.com>
2017-08-09 11:57:40 +0300

    App Events Support

--- Gemfile
+++ Gemfile
@@ -3,4 +3,4 @@ source 'https://rubygems.org'
 # Specify your gem's dependencies in omniauth-netforum-enterprise.gemspec
 gemspec
 
-gem 'netforum_enterprise', git: 'git://github.com/blueskybroadcast/netforum_enterprise.git', branch: 'master', ref: '4aedb08ad7c48029efceeb89edfa68bb4adc286e'
+gem 'netforum_enterprise', git: 'git://github.com/blueskybroadcast/netforum_enterprise.git', branch: 'show-savon-requests-and-responses', ref: '6bdd3c7fe1eb8cc4baf3ba6a9eedcab464473300'
--- lib/omniauth/strategies/netforum_enterprise.rb
+++ lib/omniauth/strategies/netforum_enterprise.rb
@@ -6,6 +6,8 @@ module OmniAuth
 
       option :name, 'netforum_enterprise'
 
+      option :app_options, { app_event_id: nil }
+
       option :client_options, {
         site: 'MUST BE SET',
         return_to_slug: nil,
@@ -37,6 +39,11 @@ module OmniAuth
       end
 
       def callback_phase
+        slug = get_slug(request.params)
+        account = Account.find_by(slug: slug)
+        @app_event = account.app_events.where(id: options.app_options.app_event_id).first_or_create(activity_type: 'sso')
+        self.env['omniauth.app_event_id'] = @app_event.id
+
         token = request.params['ssoToken'] || request.params['Token']
         if token
           self.access_token = {
@@ -45,11 +52,16 @@ module OmniAuth
           }
 
           self.env['omniauth.auth'] = auth_hash
-          self.env['omniauth.origin'] = '/' + get_slug(request.params)
+          self.env['omniauth.origin'] = '/' + slug
           call_app!
         else
+          @app_event.logs.create(level: 'error', text: "Netforum Enterprise SSO Failure: 'Token' parameter is absent!")
+          @app_event.fail!
           fail!(:invalid_credentials)
         end
+      rescue StandardError => e
+        @app_event.try(:fail!)
+        raise e
       end
 
       def creds
@@ -57,7 +69,7 @@ module OmniAuth
       end
 
       def auth_hash
-        hash = AuthHash.new(:provider => name, :uid => uid)
+        hash = AuthHash.new(provider: name, uid: uid)
         hash.info = info
         hash.credentials = creds
         hash
@@ -80,11 +92,23 @@ module OmniAuth
       def get_user_info(access_token)
         customer = {}
         client.authenticate(username, password) do |auth|
+          create_request_and_response_logs('Authentication', auth)
+
           customer_key = auth.web_validate access_token
+          create_request_and_response_logs('Web Validate', auth)
+
           customer_info = auth.get_individual_information customer_key
-          customer_committee_codes = get_user_committees(auth, customer_key)
-          customer =
-          {
+          create_request_and_response_logs('Get Individual Information', auth)
+
+          customer_committee_codes = if options.client_options.use_committee_group_sync
+                                       committees = auth.get_customer_committees customer_key
+                                       create_request_and_response_logs('Get Customer Committees', auth)
+                                       committees
+                                     else
+                                       []
+                                     end
+
+          customer = {
             id: customer_info[:cst_id],
             first_name: customer_info[:ind_first_name],
             last_name: customer_info[:ind_last_name],
@@ -94,12 +118,21 @@ module OmniAuth
             committee_codes: customer_committee_codes.map(&:cmt_code)
           }
         end
-        customer
-      end
 
-      def get_user_committees(auth, customer_key)
-        return [] unless options.client_options.use_committee_group_sync
-        auth.get_customer_committees customer_key
+        if customer[:id]
+          @app_event.update(raw_data: {
+            user_info: {
+              uid: customer[:id],
+              email: customer[:email],
+              first_name: customer[:first_name],
+              last_name: customer[:last_name]
+            }
+          })
+        else
+          @app_event.fail!
+        end
+
+        customer
       end
 
       private
@@ -127,6 +160,20 @@ module OmniAuth
       def wsdl
         options.client_options.custom_api_endpoint.presence ||
           (options.client_options.site + options.client_options.wsdl)
+      end
+
+      def provider_name
+        options.name
+      end
+
+      def create_request_and_response_logs(operation_name, client)
+        request_log_text = "#{provider_name.upcase} #{operation_name} Request:\nPOST #{client.last_request.url}, headers: #{client.last_request.headers}\n#{client.last_request.body}"
+        @app_event.logs.create(level: 'info', text: request_log_text)
+
+        response_log_text = "#{provider_name.upcase} #{operation_name} Response (code: #{client.last_response.code}):\n#{client.last_response.body}"
+        response_log_level = client.last_response.code == 200 ? 'info' : 'error'
+        @app_event.logs.create(level: response_log_level, text: response_log_text)
+        @app_event.fail! if response_log_level == 'error'
       end
     end
   end
